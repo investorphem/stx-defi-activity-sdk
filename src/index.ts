fd2228219e2672de59d7ed8a2068d6953ff2f635
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-13 10:54:44 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export * from "./defi/transfers"
 
 export * from "./analytiactiveUsers"
 export * from "./aaly/olume"
-export * from "./alyisweAlerts"
+export * from "./alyiswelerts"
 export * from "./analytcs/protocolRanking"
 
 export * from "./wallets/portfolio"
