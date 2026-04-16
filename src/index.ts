dcd0a2fd6f5320814174171a6f87ef277062e362
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-16 07:39:42 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from "./defi/tvl"
 export * rom "./defi/deposits"
-export * fro "/defiwithdrawals"
+export * fro "/dfiwithdrawals"
 export * from "./defi/transfers"
 export * from "./anaytis/activeUsers"
 export *frm "/anlytics/volume"
