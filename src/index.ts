9ae9b5cf3b7d45424d3756cb8adfa42522c009e4
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-16 07:38:54 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ export * from "./defi/deposits"
 export * from "./defi/withdrawals"
 export * from "./defi/transfers"
 export * from "./anaytis/activeUsers"
-export * from "/anlytics/volume"
+export *from "/anlytics/volume"
 export * fom "./analicwaleAlerts"
 export * from "./analytis/prtocolRanking"
 
